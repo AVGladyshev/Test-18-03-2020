b4c7c8b946fda4def9d8d24cd4df2327abcb6368
Alexandr Gladyshev <aleksandr.workgroup@gmail.com>
2020-03-18 11:14:23 +0300

    Test page

---
+++ index.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	 <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<title>Portfolio</title>
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<!-- Custom CSS -->
+	<link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<!--Startp project  -->
+
+<!-- header -->
+<header>
+  <div class="navbar navbar-dark bg-dark shadow-sm py-3">
+    <div class="container align-items-start">
+    	<svg class="d-none d-md-block" height="50" width="55">
+  			<circle cx="25" cy="25" r="25" fill="white" />
+  			<image xlink:href="img/ava.png" width="47" height="45"/>  			
+		  </svg> 
+        <a href="index.php" class="d-flex flex-column mr-auto pb-2 text-white pLeft">
+          <strong>Гладышев Александр</strong>
+          <sub class="text-light">Web-разработчик/Вёрстка</sub> 
+        </a>              	     
+    </div>
+  </div>
+</header>
+
+<!-- main section -->
+<main role="main">
+  <section class="jumbotron">
+    <div class="container text-left">
+    	<div class="row">       
+			  <div class="col-md">
+		    	<h1>О моих навыках</h1>
+    			<p class="lead text-muted">В альбоме представленны несколько работ</p>
+    			<ul>
+      			<li>Редактирование вёрстки: базовые знания HTML, CSS, Bootstrap, JavaScript, jQuery, AJAX;</li>
+	       		<li>Редактирование и написание PHP: основы работы с массивами, циклами, строками;</li>
+	       		<li>Создание таблиц PhpMyAdmin: базовые навыки работы MySQL;</li>
+	       		<li>Создание векторных изображений: коллекций иконок по ТЗ;</li>
+	      		<li>Создание шаблонов для презентаций MS PowerPoint;</li>
+     			</ul>
+		    </div>
+		    <div class="col-md-6 d-none d-lg-block ">
+          <div class="row">
+            <img class="align-self-start imgPoz" src="img/laptop-clipart-logo.png" alt="Картинка" width="520" height="280">
+            <img class="align-self-start typingSimulation"></img>
+            <img class="align-self-start imgBg"></img> 
+          </div>        
+        </div>           
+		  </div>   	 	
+    </div>
+  </section>
+
+  <div class="album bg-light" id="myAnchor">
+    <div class="container">
+      <div class="row">
+        <div class="col py-4">
+          <h2 class="text-center">Портфолио</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+          	<img id="1"  src="img/VELOCITY_pre.png" class="bd-placeholder-img card-img-top" width="100%" height="100%" alt="Cinque Terre">
+            <div class="card-body">
+              <p class="card-text">Лендинг Велосити</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a href="goods/goods.php" class="btn btn-sm btn-outline-secondary imgBtn">Посмотреть</a>
+                </div>                      
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <img id="2"  src="img/SMitler_pre.png" class="bd-placeholder-img card-img-top" width="100%" height="100%" alt="Cinque Terre">
+            <div class="card-body">
+              <p class="card-text">Лендинг Рено</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a href="goods/renault.php" class="btn btn-sm btn-outline-secondary imgBtn">Посмотреть</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <img id="4"  src="img/test_pre.png" class="bd-placeholder-img card-img-top" width="100%" height="100%" alt="Cinque Terre">
+            <div class="card-body">
+              <p class="card-text">Тестовый опрос</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                 <a href="goods/test quiz.php" class="btn btn-sm btn-outline-secondary imgBtn">Посмотреть</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+             <img id="5"  src="img/History_pre.png" class="bd-placeholder-img card-img-top" width="100%" height="100%" alt="Cinque Terre">
+            <div class="card-body">
+              <p class="card-text">Торговая компания</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a href="goods/hotel.php" class="btn btn-sm btn-outline-secondary imgBtn">Посмотреть</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+             <img id="6"  src="img/vectorMap_pre.png" class="bd-placeholder-img card-img-top" width="100%" height="100%" alt="Cinque Terre">
+            <div class="card-body">
+              <p class="card-text">Векторная карта</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a href="goods/vektor map.php" class="btn btn-sm btn-outline-secondary imgBtn">Посмотреть</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+             <img id="7"  src="img/icon collection_pre.png" class="bd-placeholder-img card-img-top" width="100%" height="100%" alt="Cinque Terre">
+            <div class="card-body">
+              <p class="card-text">Векторные иконки</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <a href="goods/vector icons.php" class="btn btn-sm btn-outline-secondary imgBtn">Посмотреть</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>
+</div>
+</main>
+
+<!-- About -->
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <h2 class="col-md-5 offset-md-7 py-4">Обо мне</h2>
+    </div>    
+  </div>
+  <div class="row justify-content-start">
+    <div class="col-4 d-none d-lg-block">
+      <div class="row">
+        <div class="col align-self-center">
+          <svg  viewBox="0 0 380 150" xmlns="http://www.w3.org/2000/svg" class="d-none d-md-block" height="150" width="300">       
+            <image style="overflow:visible;enable-background:new;" width="290" height="200" xlink:href="img/dialog.png"  transform="matrix(1.2 0 0 1 40 0)">
+            </image>
+
+            <text>
+              <tspan x="70" y="60" font-weight="bold">"Жизнью управляет не мудрость,</tspan>
+              <tspan x="70" y="85" font-weight="bold">а везение."</tspan>
+              <tspan x="180" y="110" font-weight="bold">-Marcus Tullius Cicerō</tspan>
+            </text>              
+          </svg>      
+        </div>   
+      
+        <div class="col align-self-start">
+          <svg xmlns="http://www.w3.org/2000/svg" class="d-none d-md-block" height="70" width="75">
+            <circle cx="35" cy="35" r="35" fill="grey" />
+            <image xlink:href="img/ava.png" width="65" height="62"/>             
+          </svg> 
+        </div>
+      </div>
+
+    </div>
+    <div class="col">
+    	<p class="text_indent">
+    		В этом эссе хочу ответить на два вопроса, “зачем” - во имя чего предпринимаю столь радикальную смену деятельности и почему свой выбор стоит остановить на моей визитке.
+    	</p>
+		<p class="text_indent">
+			В фармацевтической дистрибуции получил интересный опыт достижения целей, поставленных перед бизнес-процессом. Взаимодействуя с коллективом компании и партнёрами бизнеса, лекарственные средства в срок доставляют нуждающимся в лечении. Этот процесс вырабатывает высокий уровень ответственности, обязательность, упорство, методичность и внимательность. Тренируются личные качества ежедневно базовыми упражнениями к примеру, контролем взаиморасчётов, анализом остатков, выполнением плана. Как в любом спортивном зале снаряды для тренировки устаревают или приходят в негодность, после заменяются новыми. Автоматизация бизнес-процессов в фармдистрибуции подтолкнула на изучение новой для меня области знаний Веб-разработки.
+		</p>
+		<p class="text_indent">
+			Методичный подход помогает в сегментировании изучаемой информации, упорство в поиске информации и решение задач, обязательность знания всей экосистемы подталкивает к изучению окружения. Внимательное чтение отладочной консоли уменьшает срок исправления ошибок. Сейчас изучаю технологии, связанные с фронтенд-разработкой и взаимодействие с бэкенд, планирую лучше изучить серверную часть и базы данных. Ответы на многие вопросы я не знаю, но потратив время, найду ответ, напишу код после перепишу его и с вероятностью 1/6 получится элегантное решение.
+		</p>
+		<p class="text_indent">
+			Выбор команды с которой предстоит работать одинаково важен для капитана и бакового матроса в интересах комфортного взаимодействия команды.  
+		</p>
+      </p>
+    </div>
+  </div>
+</div>
+
+<!-- Contact -->
+<div class="container">
+
+    <div class="row">
+      <div class="col my-4">
+        <h2 class="text-center">Напишите мне</h2>
+      </div>    
+    </div>
+
+ <!-- Mail form -->
+  <form id="mail_form" action="">
+    <div class="form-group">
+
+      <div class="form-group col-md-6 offset-md-3 input-group mb-4 px-1">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="basic-addon1">~</span>
+        </div>
+        <input type="text" pattern="^[А-Я][а-я]+$" class="form-control" placeholder="Имя" aria-label="Username" aria-describedby="basic-addon1" name="name" required="required">
+      </div>
+
+      <div class="form-group col-md-6 offset-md-3 input-group mb-4 px-1">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="basic-addon1">~</span>
+        </div>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Введите email" aria-label="Username" aria-describedby="basic-addon1" name="email" required="required">
+      </div>   
+
+      <div class="form-group col-md-6 offset-md-3 input-group mb-3 px-1">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="basic-addon1">8</span>
+        </div>
+        <input type="tel" pattern="^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$" class="form-control" aria-describedby="phonelHelp" placeholder="Телефон" aria-label="Username" aria-describedby="basic-addon1" name="phone" required="required">    
+        <small id="phonelHelp" class="col-12 form-text text-muted">Вводите номер без восьмёрки</small>    
+      </div> 
+
+      <div class="form-group col-md-6 offset-md-3 input-group mb-3 px-1">
+        <div class="input-group-prepend">
+          <span class="input-group-text font-weight-light">Cообщение</span>
+        </div>
+         <textarea class="form-control" aria-label="With textarea" name="message" required="required"></textarea>
+      </div>
+
+        <div class="form-group col align-self-center text-center my-4">
+           <button class="btn btn-secondary" type="submit">Отправить</button>
+        </div>
+
+        <!-- Mail reply -->
+        <div class="form-group col-md-6 offset-md-3" id="result_form"></div>
+
+      </div>
+
+    </form>
+
+  </div>
+</div>
+
+<!-- footer -->
+<footer class="bg-dark shadow-sm align-self-center text-light">
+  <!-- Footer Elements -->
+  <div class="row">
+    <div class="col align-self-center text-center text-nowrap mt-3">
+      <a class="px-3" href="https://www.facebook.com/profile.php?id=100009902131833">
+        <img class="fb-icon" src="img/ico fb.png" alt="Facebook" title="Мой Facebook">
+      </a> 
+      <a class="px-3" href="https://www.linkedin.com/in/algladyshev/">
+        <img class="fb-icon" src="img/ico Linkidin.png" alt="LinkedIn" title="Мой LinkedIn">
+      </a>
+      <a class="px-3" href="https://github.com/AVGladyshev">
+        <img class="fb-icon" src="img/ico github.png" alt="Github" title="Мой Github">
+      </a>           
+    </div>      
+  </div>
+  <!-- Copyright -->
+  <div class="row">       
+      <div class="col align-self-center text-center text-nowrap my-3">© <?php echo date ( 'Y' )?>
+        <a class="text-light" href="#">Гладышев Александр</a>
+      </div>
+  </div>
+</div>
+</footer>
+
+<!-- Anchor up -->
+  <div id="top" >
+    <svg xmlns="http://www.w3.org/2000/svg" height="70" width="75">
+      <circle cx="25" cy="28" r="25" fill="white" />
+      <image xlink:href="img/up.png" width="50" height="50" alt="В верх" title="В начало"/>             
+    </svg> 
+  </div>
+
+
+<!-- End project -->
+
+	<!-- Boostrap -->
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <!-- Ajax -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
+  
+  <!-- Custom script -->
+	<script src="js/javaScript.js"></script>
+
+</body>
+</html>
